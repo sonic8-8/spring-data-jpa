a08c3ec89a8e0dd850e62edddac4a39209214be3
이승환 <seaung13@gmail.com>
2025-02-16 22:52:45 +0900

    feat: 쿼리 메서드 기능 - JPA Hint, Lock 예시 추가

--- src/main/java/study/springdatajpa/repository/MemberRepository.java
+++ src/main/java/study/springdatajpa/repository/MemberRepository.java
@@ -1,13 +1,12 @@
 package study.springdatajpa.repository;
 
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.springdatajpa.entity.Member;
 
@@ -40,5 +39,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @EntityGraph(attributePaths = {"team"})
     @Query("select m from Member m")
     List<Member> findMemberEntityGraph();
+
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findLockByUsername(String username);
 
 }
