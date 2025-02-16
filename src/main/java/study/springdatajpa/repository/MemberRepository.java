1094f4bfc54e3733b135eff9539982681fd8e8d1
이승환 <seaung13@gmail.com>
2025-02-16 21:02:23 +0900

    feat: 쿼리 메서드 기능 (2) 메서드 명으로 JPA NamedQuery 호출 - Memeber.findByUsername()

--- src/main/java/study/springdatajpa/repository/MemberRepository.java
+++ src/main/java/study/springdatajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.springdatajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.springdatajpa.entity.Member;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username")
+    List<Member> findByUsername(@Param("username") String username);
 }
