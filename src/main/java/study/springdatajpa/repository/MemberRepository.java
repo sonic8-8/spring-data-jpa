6f5270aa3ee90cf08846fadd908cb14d8a5547f9
이승환 <seaung13@gmail.com>
2025-02-16 21:23:18 +0900

    feat: 쿼리 메서드 기능 (3) @Query 어노테이션으로 리포지토리 인터페이스에 쿼리 직접 정의 - findUser()

--- src/main/java/study/springdatajpa/repository/MemberRepository.java
+++ src/main/java/study/springdatajpa/repository/MemberRepository.java
@@ -11,6 +11,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
-    @Query("select m from Member m where m.username = :username")
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
