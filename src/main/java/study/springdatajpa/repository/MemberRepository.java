dbac8c8ac5003a2c497d8a86df4e22682807e6cd
이승환 <seaung13@gmail.com>
2025-02-16 20:17:13 +0900

    feat: 쿼리 메서드 기능 (1) 메소드 명으로 쿼리 생성 - findByUsernameAndAgeGreaterThat(String username, int age)

--- src/main/java/study/springdatajpa/repository/MemberRepository.java
+++ src/main/java/study/springdatajpa/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package study.springdatajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.springdatajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
